598c6dbebe845a01eb07c4ef51a12a52ab04dc4d
Tommi Askola <tommi.askola@gofore.com>
2021-11-20 14:14:18 +0200

    add category schema

---
+++ backend/models/category.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const categorySchema = new mongoose.Schema({
+    name: String
+})
+
+categorySchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Category', categorySchema)
